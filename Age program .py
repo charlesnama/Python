111bc5ee00df650dd38163c08e971f3f02adfec4
Charles Nama <ca.nama@yahoo.com>
2022-02-23 22:48:34 +0100

    Add files via upload
    minor changes

--- Age program .py
+++ Age program .py
@@ -1,24 +1,31 @@
-print("Welcome to our age app")
-str(input("what is your name :"))
 
-
-
-def aid():
-    age = input("wat is your age: ")
-    aidc = int(age)*365
-    print("You are {} days old". format(aidc))
-aid()
-
-
-def aih():
-    age= input("what is your age: ")
-    aihc = int(age)*365*24
-    print("You are {} hours old".format(aihc))
-aih()
-
-
+print("Welcome to Age App\n")
+print("Login Below\n")
+name= (input("what is your name : "))
+print('\n Welcome {} !!!'.format(name))
 def aim():
-    age= input("what is your age: ")
+    age= input("\n what is your age: ")
     aimc = int(age)*365*24*60
-    print("You are {} minutes old".format(aimc))
-aim()
+    aidc = int(age)*365
+    aihc= int(age)*365*24
+    print("\n You can have the results in - Days(d), - Hours(h) or - Minutes(m) \n")
+    choice = input("Please input d/h/m \n ")
+    if(choice=='d'):
+        print('You are {} days old'.format(aidc))
+    if(choice=='h'):
+        print("You are {} hours old".format(aihc))
+    if(choice=='m'):
+        print('You are {} days old'.format(aimc))
+
+aim() 
+print("Are you satisfied with this approximation or you want exact figures")
+ans=input("Yes or No ")
+if(ans=='Yes'):
+    print("Thank you for using our app")
+if(ans=='No'):
+    Year=input("In what year where you born :")
+    Month=input("In what month were you born :")
+    Day= input("on what day were you born: ")
+    dob= Day+' '+ Month+ ' '+Year 
+    print(dob)
+
